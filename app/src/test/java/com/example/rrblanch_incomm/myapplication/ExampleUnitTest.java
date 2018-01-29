8955b8a222822ba70e1ea90a03df3248d720802d
Brice Blanch <bricerb@gmail.com>
2018-01-29 12:54:40 -0500

    more init

--- app/src/test/java/com/example/rrblanch_incomm/myapplication/ExampleUnitTest.java
+++ app/src/test/java/com/example/rrblanch_incomm/myapplication/ExampleUnitTest.java
@@ -14,4 +14,14 @@ public class ExampleUnitTest {
     public void addition_isCorrect() throws Exception {
         assertEquals(4, 2 + 2);
     }
+
+    @Test
+    public void shouldPass() throws Exception {
+        assertEquals(1,1);
+    }
+
+    @Test
+    public void shouldFail() throws Exception {
+        assertEquals(1,2);
+    }
 }
